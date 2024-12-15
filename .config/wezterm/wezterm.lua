4126835fd384e1f3320b94c38114ef22b2bdb9bd
Wanderlei Rodrigo Pereira <wrp@toriamamac.local>
2024-12-15 18:30:30 -0300

    gitignore

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,49 +1,14 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+local config = require("config")
+require("events")
 
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- Apply color scheme based on the WEZTERM_THEME environment variable
+local themes = {
+	nord = "Nord (Gogh)",
+	onedark = "One Dark (Gogh)",
 }
-
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 14
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
-config.enable_tab_bar = false
-config.window_background_image = "/Users/wrp/Pictures/nasa--hI5dX2ObAs-unsplash.jpg"
-config.window_background_image_hsb = {
-	brightness = 0.01,
-	hue = 1.0,
-	saturation = 0.5,
-}
-config.keys = {
-	{
-		key = "f",
-		mods = "CTRL",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "'",
-		mods = "CTRL",
-		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-	},
-}
-config.mouse_bindings = {
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = wezterm.action.OpenLinkAtMouseCursor,
-	},
-}
+local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
+local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
+config.color_scheme = themes[selected_theme]
 
 return config
