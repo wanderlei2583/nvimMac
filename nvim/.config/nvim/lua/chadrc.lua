f58cc8163d6ead582d57d74f8a12f7d9dec34b50
Wanderlei Rodrigo Pereira <wrp@toriamamac.local>
2024-12-16 22:29:24 -0300

    italico nas funcoes

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -2,13 +2,15 @@
 local M = {}
 
 M.base46 = {
-  theme = "catppuccin",
-  transparency = true,
+	theme = "catppuccin",
+	transparency = true,
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-  },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+		Function = { italic = true },
+		["@function"] = { italic = true },
+	},
 }
 
 return M
